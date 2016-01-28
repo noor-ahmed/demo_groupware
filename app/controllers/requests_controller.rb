747d9e1e31e4f9ff5f5af68947a1ffe2d44fcf27
gittiggit <mailtommrr@gmail.com>
2016-01-28 12:49:52 +0600

    Commited after creating a user

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,12 @@
+class RequestsController < ApplicationController
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'requests/index'
+
+  get 'requests/new'
+
+  get 'requests/show'
+
   get 'welcomes/about'
   get 'welcomes/contact'
   root "welcomes#home"
